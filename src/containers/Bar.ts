08fe2478ad7e38cf875e5737b6b54f02b9d8ef70
David Sferruzza <david.sferruzza@gmail.com>
2018-11-16 17:46:26 +0100

    Refactor calendar fetching

--- src/containers/Bar.ts
+++ src/containers/Bar.ts
@@ -1,48 +1,25 @@
-import fetch from 'cross-fetch';
-import * as queryString from 'query-string';
 import { connect } from 'react-redux';
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 
 import Bar from '../components/Bar';
+import { fetchCalendar } from '../lib/calendar';
 import { FetchCalendar, FetchCalendarError, ReceiveCalendar, StoreState } from '../lib/state';
 
 type Action = FetchCalendar | FetchCalendarError | ReceiveCalendar;
 type ThunkResult<R> = ThunkAction<R, StoreState, undefined, Action>;
 type MyThunkDispatch = ThunkDispatch<StoreState, undefined, Action>;
 
-function fetchCalendar(): ThunkResult<void> {
+function updateCalendar(): ThunkResult<void> {
   return async (dispatch, getState) => {
     const accessToken = getState().config.accessToken;
     if (accessToken !== '') {
       const timeMin = getState().config.timeMin;
-      const qs = {
-        access_token: accessToken,
-        maxResults: 2500,
-      };
-      if (timeMin !== '') {
-        Object.assign(qs, { timeMin });
-      }
-      const url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?' + queryString.stringify(qs);
-      const options: RequestInit = {
-        headers: new Headers({
-          'Content-Type': 'application/json',
-        }),
-        mode: 'cors',
-      };
-      return fetch(url, options)
-        .then((res: Response) => {
-          if (res.status === 200) {
-            return res.text();
-          } else {
-            return Promise.reject(`La récupération du calendrier a renvoyé le code d'erreur ${res.status} (${res.statusText})`);
-          }
-        })
-        .then((data: string) => {
-          dispatch({ type: 'ReceiveCalendar', calendarPayload: data });
-        }, (error: Error | string) => {
-          const msg: string = (error instanceof Error) ? `${error.name}: ${error.message}` : error;
-          dispatch({ type: 'FetchCalendarError', msg })
-        });
+      fetchCalendar(accessToken, timeMin).then((data: string) => {
+        dispatch({ type: 'ReceiveCalendar', calendarPayload: data });
+      }, (error: Error | string) => {
+        const msg: string = (error instanceof Error) ? `${error.name}: ${error.message}` : error;
+        dispatch({ type: 'FetchCalendarError', msg })
+      });
     } else {
       dispatch({ type: 'FetchCalendarError', msg: "Le token d'accès est vide !" });
     }
@@ -53,7 +30,7 @@ function mapDispatchToProps(dispatch: MyThunkDispatch) {
   return {
     onReloadClick: () => {
       dispatch({ type: 'FetchCalendar' });
-      dispatch(fetchCalendar());
+      dispatch(updateCalendar());
     },
   };
 }
---
+++ src/lib/calendar.ts
@@ -0,0 +1,26 @@
+import fetch from 'cross-fetch';
+import * as queryString from 'query-string';
+
+export function fetchCalendar(accessToken: string, timeMin: string): Promise<string> {
+  const qs = {
+    access_token: accessToken,
+    maxResults: 2500,
+  };
+  if (timeMin !== '') {
+    Object.assign(qs, { timeMin });
+  }
+  const url = 'https://www.googleapis.com/calendar/v3/calendars/primary/events?' + queryString.stringify(qs);
+  const options: RequestInit = {
+    headers: new Headers({
+      'Content-Type': 'application/json',
+    }),
+    mode: 'cors',
+  };
+  return fetch(url, options).then((res: Response) => {
+    if (res.status === 200) {
+      return res.text();
+    } else {
+      return Promise.reject(`La récupération du calendrier a renvoyé le code d'erreur ${res.status} (${res.statusText})`);
+    }
+  });
+}
