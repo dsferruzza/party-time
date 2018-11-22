66db4815cd78b9b5fca97980f456536762bb185e
David Sferruzza <david.sferruzza@gmail.com>
2018-11-22 18:11:14 +0100

    Improve homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@material-ui/core';
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import SettingsIcon from '@material-ui/icons/Settings';
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const styles = createStyles({
 });
@@ -19,6 +22,7 @@ function Home(props: Props) {
         Calcul des congés et jours non travaillés dans le cadre d'un travail à temps partiel.
       </Typography>
       <hr />
+      {(!props.configured) ? <Link to="/config"><Button variant="contained" color="default">Configurer <SettingsIcon /></Button></Link> : null}
     </div>
   )
 }
